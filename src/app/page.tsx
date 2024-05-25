c9d7b59de51279c471ac68f8ac3d0a5f2dfd27d2
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2024-05-25 13:56:47 +0100

    DAy 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import ProductList from "@/components/ProductList";
+import Slider from "../components/Slider";
+import CategoryList from "@/components/CategoryList";
+
 const HomePage = () => {
   return (
-    <div className=''>HomePage</div>
-  )
-}
+    <div className="">
+      <Slider />
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
+        <ProductList />
+      </div>
+      <div className="mt-24 ">
+        <h1 className="text-2xl font-bold  px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <ProductList />
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import CartModal from "./CartModal";
+
+const NavIcons = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+  const isLoggedIn = false;
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen(!isProfileOpen);
+  };
+  return (
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        // onClick={login}
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/profile">Profile</Link>
+          <div className="mt-2 cursor-pointer">
+            {isLoading ? "Logging out" : "Logout"}
+          </div>
+        </div>
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <div>
+        <Image
+          src="/cart.png"
+          alt=""
+          width={22}
+          height={22}
+          className="cursor-pointer"
+          onClick={() => setIsCartOpen(!isCartOpen)}
+        />
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
+          6
+        </div>
+      </div>
+      {isCartOpen && <CartModal />}
+    </div>
+  );
+};
+
+export default NavIcons;
